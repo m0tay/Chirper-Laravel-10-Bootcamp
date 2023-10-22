93337aad1e765503e1a9b5e00abc4d563afbc0f0
m0tay <mail.roi.douglas@gmail.com>
2023-10-22 19:42:29 +0100

    ff

--- resources/js/Pages/Dashboard copy.vue
+++ resources/js/Pages/Dashboard copy.vue
@@ -1,8 +1,6 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
-import TextInput from '@/Components/TextInput.vue';
-import Modal from '@/Components/Modal.vue';
 </script>
 
 <template>
@@ -17,9 +15,6 @@ import Modal from '@/Components/Modal.vue';
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900">You're logged in!</div>
-                </div>
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg mt-2">
-                    <div class="p-6 text-gray-900">Agenda: #</div>
                 </div>
             </div>
         </div>
